279a263f696f896c9294d6f381bbf7b74fd4130e
= <admin@ryancollins.io>
2016-06-14 22:58:47 -0400

    Feat: add intro image

--- app/src/components/Intro/Intro.js
+++ app/src/components/Intro/Intro.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './Intro.scss'
 import ChevronDown from 'react-icons/lib/fa/chevron-down';
-import Me from '../../../images/me.jpg';
+import IntroImage from './IntroImage';
 
-const MeImage = () => (
-  <img
-    src={Me}
-  />
-)
 
 const Intro = props => (
   <div className="intro">
     <div className="intro-header">
       <div className="intro-header__content">
-        <MeImage />
+        <IntroImage />
         <h1 className="intro-header__headline">I am Ryan Collins</h1>
         <h4 className="intro-header__subheadline">I make Web and Mobile Apps</h4>
         <ChevronDown className="intro-header__chevron-down"></ChevronDown>
---
+++ app/src/components/Intro/IntroImage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Me from '../../../images/me.jpg';
+import './IntroImage.scss';
+
+const IntroImage = () => (
+  <img
+    src={Me}
+    className="intro-image"
+  />
+)
+
+export default IntroImage;
