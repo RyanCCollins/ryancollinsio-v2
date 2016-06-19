9253261b84357e2ef076027f7293fad008bd6556
Ryan Collins <rccproductions@mac.com>
2016-06-18 22:37:50 -0400

    Feat: add interpolating styles to circle chart

--- app/src/components/CircleChart/CircleChart.js
+++ app/src/components/CircleChart/CircleChart.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './CircleChart.scss';
-import { Motion, spring } from 'react-motion';
+import { StaggeredMotion, spring } from 'react-motion';
 
 class CircleChart extends React.Component {
   constructor(props) {
     super(props);
+    this.setItemHeight = this.setItemHeight.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
     this.state = {
-      circleStyle: {
-        height: 0
-      }
+      height: 0
     };
-    this.getItemHeight = this.getItemHeight.bind(this);
   }
   componentDidMount() {
-    this.getItemHeight();
+    window.addEventListener('scroll', this.handleScroll);
   }
-  getItemHeight() {
+  setItemHeight() {
     const height = 108 * (this.props.item.percent / 100);
     this.setState({
       height
     });
+  }
+  handleScroll(event) {
+    this.setItemHeight();
   }
   render() {
     const {
@@ -27,20 +29,27 @@ class CircleChart extends React.Component {
     } = this.props;
     return (
       <div className="item last" id={`language-item-${item.id}`}>
-        <div className="circle">
-          <Motion
-            default={{height: 0}}
-            style={{ height: spring(this.state.height) }}
-          >
-            {interpolatingStyle =>
-              <span
-                className="item-progress"
-                style={interpolatingStyle}
-                data-percent={item.percent}
-              ></span>
-            }
-          </Motion>
-        </div>
+        <StaggeredMotion
+          defaultStyles={[{ height: 0 }]}
+          styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+            return i === 0 ?
+              { height: spring(this.state.height) }
+            :
+              { height: spring(prevInterpolatedStyles[i - 1].height) }
+        })}>
+          {interpolatingStyles =>
+            <div className="circle">
+              {interpolatingStyles.map((style, i) =>
+                <span
+                  className="item-progress"
+                  key={i}
+                  style={style}
+                  data-percent={item.percent}
+                ></span>
+            )}
+            </div>
+          }
+        </StaggeredMotion>
         <span className="percent">{item.percent}%</span>
         <h4 className="text-center uppercase">{item.name}</h4>
       </div>
