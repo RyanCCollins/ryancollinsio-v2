ecfe956cbe994ed81074cbf58892ae5cdc4c94c2
Ryan Collins <rccproductions@mac.com>
2016-07-03 22:01:07 -0400

    Feat: add loading indicator for loading up the portfolio

--- app/src/components/Portfolio/PortfolioGrid.js
+++ app/src/components/Portfolio/PortfolioGrid.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import PortfolioItem from './PortfolioItem';
 import { Column } from 'react-foundation';
 
 const PortfolioGrid = ({
-  projects
+  projects,
+  onLoad
 }) => (
   <div className="portfolio-grid">
     <Column isColumn large={12} centerOnSmall>
       {projects.map((project) =>
         <PortfolioItem
           project={project}
+          onLoad={onLoad}
           key={project.id}
         />
       )}
     </Column>
   </div>
 );
+
+PortfolioGrid.propTypes = {
+  projects: PropTypes.array.isRequired,
+  onLoad: PropTypes.func.isRequired
+};
 
 export default PortfolioGrid;
--- app/src/containers/Portfolio/Portfolio.js
+++ app/src/containers/Portfolio/Portfolio.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import PortfolioGrid from '../../components/Portfolio/PortfolioGrid';
-import { Divider } from '../../components';
+import { Divider, LoadingIndicator } from '../../components';
 
-const Portfolio = (props) => (
-  <div className="portfolio-container">
-    <h1 className="section-header text-grey portfolio__section-header">Portfolio</h1>
-    <Divider />
-    <PortfolioGrid {...props} />
-  </div>
-);
+const Styles = {
+  hidden: {
+    display: 'none'
+  },
+  notHidden: {
+    display: ''
+  }
+};
+
+class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.handleEndLoad = this.handleEndLoad.bind(this);
+    this.state = {
+      didLoad: false
+    };
+  }
+  componentDidMount() {
+    const {
+      didLoad
+    } = this.state;
+    if (!didLoad) {
+      this.state = {
+        isLoading: true
+      };
+    }
+  }
+  handleEndLoad() {
+    this.setState({ isLoading: false, didLoad: true });
+  }
+  render() {
+    const {
+      isLoading
+    } = this.state;
+    return (
+      <div>
+        <LoadingIndicator isLoading={isLoading} />
+        <div className="portfolio-container" style={isLoading ? Styles.hidden : Styles.notHidden}>
+          <h1 className="section-header text-grey portfolio__section-header">Portfolio</h1>
+          <Divider />
+          <PortfolioGrid
+            {...this.props}
+            onLoad={this.handleEndLoad}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Portfolio;
