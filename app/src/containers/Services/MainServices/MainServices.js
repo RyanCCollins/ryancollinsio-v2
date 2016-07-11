7965ce2f3cbd136eb18a480e0ed69d71b2977208
Ryan Collins <techrapportrc@gmail.com>
2016-07-11 16:05:11 +0000

    Feat: add map state to props for services

--- app/src/containers/Services/MainServices/MainServices.js
+++ app/src/containers/Services/MainServices/MainServices.js
@@ -4,39 +4,6 @@ import {
   Row
 } from 'react-foundation';
 
-const services = [
-  {
-    id: 0,
-    title: 'Progressive Web',
-    description: 'With years of experience and expertise building progressive web software built for the Internet of Things. I have built software in use in appliances and smart products across the world and can help you to build out your product using the lateset and greatest web technologies available today.'
-  },
-  {
-    id: 1,
-    title: 'Mobile',
-    description: 'I focus on creating web software with a mobile-first approach using responsive web technologies and progressive JavaScript frameworks. Utilizing Swift for native iOS applications and React Native, I can build you pixel perfect mobile applications.'
-  },
-  {
-    id: 2,
-    title: 'Big Data / Machine Learning',
-    description: 'With experience in big data and Deep Learning, I can help make your software smarter.'
-  },
-  {
-    id: 3,
-    title: 'Data Architecture',
-    description: 'You don\'t need to be Google to employ an intelligent cloud computing system. With nearly 8 years experience architecting scalable data systems, I can help you to implement the latest and greatest real-time distributed server technologies, including Docker, MongoDB, SQL, Puppet, Chef, et. al.'
-  },
-  {
-    id: 4,
-    title: 'Agile Project Management',
-    description: 'Agile software development helps you to get software into the hands of your end users faster. Utilizing Scrum and other Agile methodologies, I can help you to keep your project moving on schedule.'
-  },
-  {
-    id: 5,
-    title: 'Pro Bono Consulting',
-    description: 'Through the <a href="https://hacksmiths.io">Hacksmiths.io</a> platform, I offer Pro Bono software development and consulting services to charitable and philanthrophic organizations. Please contact me if your charitable organization can use the help of a talented group of professional software developers!'
-  }
-];
-
 const MainServiceItem = ({
   item
 }) => (
--- app/src/containers/ServicesPage/ServicesPage.js
+++ app/src/containers/ServicesPage/ServicesPage.js
@@ -4,7 +4,8 @@ import {
   ServicesSectionTwo,
   ServicesSectionOne,
   VideoSeries,
-  Team
+  Team,
+  MainServices
 } from 'containers';
 import {
   Column,
@@ -12,6 +13,7 @@ import {
   Button
 } from 'react-foundation';
 import './ServicesPage.scss';
+import { connect } from 'react-redux';
 
 const ServicesPage = props => (
   <div className="services-page-wrapper">
@@ -28,10 +30,9 @@ const ServicesPage = props => (
       >
       <div className="services-intro">
         <p>Included on this page are some of the services that I offer as an independant consultant.</p>
-        <p>In some cases, I may try to connect you to one of my colleagues if you are seeking a specific set of skills that I don't have.</p>
-        <p>Head on over to <a href="http://hacksmiths.io">Hacksmiths.io</a> for more information about the Hacksmiths Platform and what we do.</p>
       </div>
       </Column>
+      <MainServices services={props.services} />
       <Column
         large={8}
         medium={10}
@@ -54,4 +55,12 @@ const ServicesPage = props => (
   </div>
 );
 
-export default ServicesPage;
+ServicesPage.propTypes = {
+  services: PropTypes.array.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  services: state.services
+});
+
+export default connect(mapStateToProps)(ServicesPage);
