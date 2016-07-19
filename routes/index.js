f1d032503f7106702f20ba25b7741799dcd0e359
Ryan Collins <rccproductions@mac.com>
2016-07-19 14:41:48 -0400

    Fix: remove routes from server rendering

--- routes/index.js
+++ routes/index.js
@@ -60,24 +60,24 @@ exports = module.exports = function (app) {
 
   app.all('/api/contact/create', routes.api.contact.create);
 
-  app.use((req, res) => {
-    // Note that req.url here should be the full URL path from
-    // the original request, including the query string.
-    match({ routes: serverRoutes, location: req.url }, (error, redirectLocation, renderProps) => {
-      if (error) {
-        res.status(500).send(error.message);
-      } else if (redirectLocation) {
-        res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-      } else if (renderProps) {
-        // You can also check renderProps.components or renderProps.routes for
-        // your "not found" component or route respectively, and send a 404 as
-        // below, if you're using a catch-all route.
-        res.status(200).send(renderToString(<RouterContext {...renderProps} />));
-      } else {
-        res.status(404).send('Not found');
-      }
-    });
-  });
+  // app.use((req, res) => {
+  //   // Note that req.url here should be the full URL path from
+  //   // the original request, including the query string.
+  //   match({ routes: serverRoutes, location: req.url }, (error, redirectLocation, renderProps) => {
+  //     if (error) {
+  //       res.status(500).send(error.message);
+  //     } else if (redirectLocation) {
+  //       res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+  //     } else if (renderProps) {
+  //       // You can also check renderProps.components or renderProps.routes for
+  //       // your "not found" component or route respectively, and send a 404 as
+  //       // below, if you're using a catch-all route.
+  //       res.status(200).send(renderToString(<RouterContext {...renderProps} />));
+  //     } else {
+  //       res.status(404).send('Not found');
+  //     }
+  //   });
+  // });
 
 
   app.use(express.static('./public'));
