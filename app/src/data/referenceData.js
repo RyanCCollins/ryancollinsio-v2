fb547aad6c654b507ff7360132679cfd54ce443e
Ryan Collins <rccproductions@mac.com>
2016-07-24 18:35:28 -0400

    Feat: add gabriele's reference to reference data

--- app/src/data/referenceData.js
+++ app/src/data/referenceData.js
@@ -1,6 +1,26 @@
 const references = [
   {
     id: 0,
+    text: `Ryan will be a great asset in any field he
+      applies his energy to. He is extremely knowledgeable
+      and has also some fundamental traits that make him ideal in
+      any team. He is very open minded, determined and well rounded
+      in javascript, Front-End and Back-End. He did a tremendous job
+      coordinating a team of developers from different parts of the
+      globe to achieve results within a deadline on the Food Drivr project.
+      I really enjoyed our hacking marathons and working hard together towards
+      a goal. Ryan was always either helping out the Front-End or the
+      Back-End team and even hopping in with the Mobile one as well.
+      His dedication and knowledge as a developer are inspiring!`,
+    person: {
+      image: 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAaFAAAAJGFiZTQxNmE5LTZhY2ItNDgwNS1iMzgyLTU4ODAxZGM0MTE0OA.jpg',
+      company: 'Food Perk',
+      name: 'Gabriele Cimato',
+      title: 'Lead Full Stack Engineer - Food Perk'
+    }
+  },
+  {
+    id: 1,
     text: `Ryan is a fantastic leader, teacher and project manager.
       He has a wealth of knowledge expanding from the Front end technologies
       to integrating the back-end aspect on projects. The information he brings
@@ -14,7 +34,7 @@ const references = [
     }
   },
   {
-    id: 1,
+    id: 2,
     text: 'Ryan is a brilliant, organized and fluent Full Stack Engineer. While working with Ryan he exemplified leadership in the areas of design, full stack engineering and web/mobile development. He has a vast range of skills and is able to pick up new technologies at the drop of a hat. I would definitely recommend this detail-oriented and qualified person for a full stack engineer or team lead position.',
     person: {
       image: 'http://techrapport.com/assets/images/references/harry.jpg',
@@ -24,7 +44,7 @@ const references = [
     }
   },
   {
-    id: 2,
+    id: 3,
     text: "Ryan has been an outstanding leader from the day that I've met him. He constantly shows initiative and well-developed problem-solving skills that have allowed him to create several large-scale projects on his own, freelance, create his own company, and even begin working towards a Hackathon. Ryan is also extremely insightful and has helped me learn quite a bit. He has pushed everyone on the Hacksmiths team to perform their best and on schedule. I believe that Ryan will succeed and compliment any development team he joins.",
     person: {
       image: 'http://techrapport.com/assets/images/references/frank.jpg',
@@ -34,7 +54,7 @@ const references = [
     }
   },
   {
-    id: 3,
+    id: 4,
     text: "Ryan is one of the most focused developers I've had the privilege to work with. His knowledge base is also incredibly deep. We have partnered on several projects and all of them are world class. Ryan is also an incredibly enjoyable person to work with and I've had a blast working with him.",
     person: {
       image: 'http://techrapport.com/assets/images/references/sean.jpg',
@@ -44,7 +64,7 @@ const references = [
     }
   },
   {
-    id: 4,
+    id: 5,
     text: "Ryan was one of the standout employees in my tenure ... More importantly, Ryan was innovative and always looking for new ways to solve problems.  He has tremendous leadership potential, both in terms of his interpersonal skills and his solid knowledge of data systems...",
     person: {
       image: 'http://techrapport.com/assets/images/references/alice-bonner.jpeg',
