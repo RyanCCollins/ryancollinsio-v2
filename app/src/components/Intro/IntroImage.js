e03f40c09c255da9e07eea83d0c6fe1c12ecac16
Ryan Collins <rccproductions@mac.com>
2016-07-24 20:33:32 -0400

    Chore: add production build

--- app/src/components/Intro/IntroImage.js
+++ app/src/components/Intro/IntroImage.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import CrownLogoImage from '../../../images/crown.png';
+import ReactLogoSrc from './react-logo.png';
+
+const ReactLogo = () => (
+  <img
+    style={{ height: 80, width: 80 }}
+    src={ReactLogoSrc}
+    alt="React Logo"
+    className="wicked-cool-react-logo"
+  />
+);
+
+const CrownLogo = () => (
+  <img
+    alt="A crown logo"
+    src={CrownLogoImage}
+    className="intro-image"
+  />
+);
 
 const IntroImage = () => (
   <div className="logo-wrap">
-    <img
-      alt="An image of a crown"
-      src={CrownLogoImage}
-      className="intro-image"
-    />
+    <CrownLogo />
     <div className="logo-text-wrap">
-      <h1 className="logo-text">RC</h1>
+      <ReactLogo />
+      {/*<h1 className="logo-text">RC</h1>*/}
     </div>
   </div>
 );
