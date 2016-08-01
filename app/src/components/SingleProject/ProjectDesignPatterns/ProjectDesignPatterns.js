46f8bc4697bc24bb67c797ab2f1477b67477f1b0
Ryan Collins <RyanCCollins@users.noreply.github.com>
2016-08-01 08:31:44 -0400

    Chore: add key to design pattern list items

--- app/src/components/SingleProject/ProjectDesignPatterns/ProjectDesignPatterns.js
+++ app/src/components/SingleProject/ProjectDesignPatterns/ProjectDesignPatterns.js
@@ -8,7 +8,13 @@ import {
 const ProjectDesignPatterns = ({
   project
 }) => (
-  <Column className="project-design-patterns-section" small={12} large={8} isColumn centerOnSmall>
+  <Column
+    className="project-design-patterns-section"
+    small={12}
+    large={8}
+    isColumn
+    centerOnSmall
+  >
     <h1 className="section-header">Design Patterns</h1>
     <h4 className="section-sub-title">Notable design patterns used in development</h4>
     <Divider />
@@ -17,8 +23,8 @@ const ProjectDesignPatterns = ({
         {project.designPatterns.description}
       </p>
       <div className="tag-label-container">
-        {project.designPatterns.tags.map((tag) =>
-          <Label className="tag-label">
+        {project.designPatterns.tags.map((tag, i) =>
+          <Label key={i} className="tag-label">
             {tag}
           </Label>
         )}
