e9e1e3a1b047e3594b5934b48cbfe278de7fdb6d
Ryan Collins <rccproductions@mac.com>
2016-08-06 20:48:30 -0400

    Chore: add production build

--- app/src/data/referenceData.js
+++ app/src/data/referenceData.js
@@ -1,6 +1,23 @@
 const references = [
   {
     id: 0,
+    text: `Ryan is one of the most intelligent Software
+      Engineers I've had the pleasure of knowing. He has demonstrated
+      his leadership skills as leader of Hacksmiths and has a personality
+      that inspires those around him to always do their best. I have
+      gained a lot of knowledge about software development from Ryan,
+      who always takes the time to explain concepts that are difficult
+      to grasp. Whether as a team member or as a leader, Ryan would be
+      an asset to any project. `,
+    person: {
+      image: 'https://github.com/RyanCCollins/cdn/blob/master/portfolio-image-gallery-images/AAEAAQAAAAAAAAP1AAAAJGY3NDVkY2MyLWE4MmMtNDEwMi1hZWM4LWYxNTIzYjA2Y2EyMw.jpg?raw=true',
+      company: 'Hacksmiths',
+      name: 'Mikael Mukhsikaroyan',
+      title: 'iOS Engineer - Hacksmiths'
+    }
+  },
+  {
+    id: 10,
     text: `Ryan will be a great asset in any field he
       applies his energy to. He is extremely knowledgeable
       and has also some fundamental traits that make him ideal in
@@ -20,21 +37,21 @@ const references = [
     }
   },
   {
-    id: 1,
+    id: 20,
     text: `Ryan is a fantastic leader, teacher and project manager.
       He has a wealth of knowledge expanding from the Front end technologies
       to integrating the back-end aspect on projects. The information he brings
       fundamentally makes everyone around him better. Anytime someone is
       struggling to understand or complete a task he's available to walk through it.`,
     person: {
-      image: 'https://media.licdn.com/media/AAEAAQAAAAAAAAbiAAAAJDkyOTRjMzZiLTA2MGUtNDI5Ni1iMWEzLTI4ODIxODM3NjNkYQ.jpg',
+      image: 'https://media.licdn.com/media/AAEAAQAAAAAAAAbiAAAAJDkyOTRjMzZiLTA30MGUtNDI5Ni1iMWEzLTI4ODIxODM3NjNkYQ.jpg',
       company: 'Perry\'s Development',
       name: 'Cody Perry',
       title: 'Front End Web Developer - Perry\'s Development'
     }
   },
   {
-    id: 2,
+    id: 30,
     text: 'Ryan is a brilliant, organized and fluent Full Stack Engineer. While working with Ryan he exemplified leadership in the areas of design, full stack engineering and web/mobile development. He has a vast range of skills and is able to pick up new technologies at the drop of a hat. I would definitely recommend this detail-oriented and qualified person for a full stack engineer or team lead position.',
     person: {
       image: 'http://techrapport.com/assets/images/references/harry.jpg',
@@ -44,7 +61,7 @@ const references = [
     }
   },
   {
-    id: 3,
+    id: 40,
     text: "Ryan has been an outstanding leader from the day that I've met him. He constantly shows initiative and well-developed problem-solving skills that have allowed him to create several large-scale projects on his own, freelance, create his own company, and even begin working towards a Hackathon. Ryan is also extremely insightful and has helped me learn quite a bit. He has pushed everyone on the Hacksmiths team to perform their best and on schedule. I believe that Ryan will succeed and compliment any development team he joins.",
     person: {
       image: 'http://techrapport.com/assets/images/references/frank.jpg',
@@ -54,7 +71,7 @@ const references = [
     }
   },
   {
-    id: 4,
+    id: 50,
     text: "Ryan is one of the most focused developers I've had the privilege to work with. His knowledge base is also incredibly deep. We have partnered on several projects and all of them are world class. Ryan is also an incredibly enjoyable person to work with and I've had a blast working with him.",
     person: {
       image: 'http://techrapport.com/assets/images/references/sean.jpg',
@@ -64,7 +81,7 @@ const references = [
     }
   },
   {
-    id: 5,
+    id: 60,
     text: "Ryan was one of the standout employees in my tenure ... More importantly, Ryan was innovative and always looking for new ways to solve problems.  He has tremendous leadership potential, both in terms of his interpersonal skills and his solid knowledge of data systems...",
     person: {
       image: 'http://techrapport.com/assets/images/references/alice-bonner.jpeg',
