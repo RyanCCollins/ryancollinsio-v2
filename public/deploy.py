8f0f3654c5895cacb9bb0225004fc6f1481744a3
Ryan Collins <rccproductions@mac.com>
2016-08-22 21:44:13 -0400

    Feat: add deploy.py script, again

---
+++ public/deploy.py
@@ -0,0 +1,29 @@
+from subprocess import call
+import time
+import os
+import shutil
+
+call(["npm", "run", "deploy"])
+time.sleep(120)
+src = './app/dist'
+dest = './public'
+
+def clearOldFiles(dir):
+    old_files = os.listdir(dir)
+    for file_name in old_files:
+        full_file_name = os.path.join(dir, file_name)
+        if (os.path.isfile(full_file_name)):
+            if "deploy" not in file_name:
+                os.remove(full_file_name)
+
+
+def moveNewFiles(dir):
+    for file_name in os.listdir(dir):
+        full_file_name = os.path.join(src, file_name)
+        if (os.path.isfile(full_file_name)):
+            shutil.move(full_file_name, dest)
+
+
+def main():
+    clearOldFiles(dest)
+    moveNewFiles(src)
