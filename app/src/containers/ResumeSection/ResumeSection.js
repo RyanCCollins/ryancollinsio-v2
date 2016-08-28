65052a41f8c5ce720858dc6b84c76a48175b1d91
Ryan Collins <rccproductions@mac.com>
2016-08-28 19:02:15 -0400

    Feat: add intro paragraph to resume section

--- app/src/containers/ResumeSection/ResumeSection.js
+++ app/src/containers/ResumeSection/ResumeSection.js
@@ -14,16 +14,49 @@ const ResumeSection = ({
   resumeData
 }) => (
   <section className="resume-section section-content gradient-blue" id="resume">
-    <h1 className="section-header">Qualifications</h1>
+    <h1 className="section-header">Education</h1>
     <Divider />
     <Row className="resume-section__row-full">
       <Column small={12} medium={12} centerOnSmall>
-        <div className="description-text description-text__full-width">
-          Hello World
-        </div>
+        <Column small={12} medium={8} large={8} centerOnSmall>
+          <div className="description-text description-text__full-width">
+            <div className="description-paragraph-wrapper">
+              <h4>
+                Road to <strong>Mastery</strong>
+              </h4>
+              <p>
+                {`Through open access and from
+                    investing a tremendous amount of time and energy in education,
+                    I believe that I am at the `}
+                    <strong>forefront of the tech field</strong>.
+              </p>
+            </div>
+            <div className="description-paragraph-wrapper">
+              <h4><strong>Life-long Learning</strong></h4>
+              <p>
+                {`My educational path is to focus on Front End web mastery,
+                    while studying academic subjects such as Data Science and
+                    Human Computer Interaction.  I intend to receive my Master's
+                    and at least one certification per year.`}
+              </p>
+            </div>
+            <div className="description-paragraph-wrapper">
+              <h4><strong>Practical & Academic</strong></h4>
+              <p>
+                {`I am very intrigued by the possibilities in implementing
+                    cutting-edge computer science research into the mainstream.
+                    Particularly, I am interested in the effect of
+                    Functional Programming (Lambda Calculus) in
+                    mainstream JavaScript web development.`}
+              </p>
+            </div>
+          </div>
+        </Column>
       </Column>
     </Row>
     <div className="container">
+      <h1 className="section-header">Certifications</h1>
+      <Divider />
       <Row>
         <Column
           isColumn
