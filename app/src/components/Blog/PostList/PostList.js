9b61234483ff4f03d31330521546fe90142727c6
Ryan Collins <rccproductions@mac.com>
2016-08-30 22:43:04 -0400

    Chore: add production bundle.js

--- app/src/components/Blog/PostList/PostList.js
+++ app/src/components/Blog/PostList/PostList.js
@@ -13,17 +13,19 @@ class PostList extends React.Component {
       posts,
       onChangePage
     } = this.props;
-    const postItems = posts.reverse().map((post, ind) =>
-      <Column
-        key={ind}
-        xlarge={4}
-        large={6}
-        medium={6}
-        small={12}
-        isColumn
-      >
-        <PostListItem post={post} />
-      </Column>
+    const postItems = posts
+      .reverse()
+      .map((post, ind) =>
+        <Column
+          key={ind}
+          xlarge={4}
+          large={6}
+          medium={6}
+          small={12}
+          isColumn
+        >
+          <PostListItem post={post} />
+        </Column>
     );
     return (
       <div className="post-list">
