9e6f19001bf83de68140d70976f5b1fa7412300d
Ryan Collins <rccproductions@mac.com>
2016-09-16 19:42:47 -0400

    Feat: comment out the res.send at end of route file

--- routes/index.js
+++ routes/index.js
@@ -85,7 +85,7 @@ exports = module.exports = function (app) {
         }
       });
   });
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  });
+  // app.get('/*', (req, res) => {
+  //   res.sendFile(path.join(__dirname, 'public/index.html'));
+  // });
 };
