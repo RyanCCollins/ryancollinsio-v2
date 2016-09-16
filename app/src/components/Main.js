21fd5bce881d04be0071656fbec572184b660275
Ryan Collins <rccproductions@mac.com>
2016-09-16 18:55:46 -0400

    Fix: add sanity check for this in main.js

--- app/src/components/Main.js
+++ app/src/components/Main.js
@@ -6,7 +6,7 @@ class Main extends Component {
     return (
       <div>
         <Navbar />
-        {typeof this.props !== undefined ?
+        {typeof this !== undefined ?
           React.cloneElement(this.props.children, this.props) : null}
       </div>
     );
