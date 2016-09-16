511982605710b03f1f9a4429e879f75507a38e6f
Ryan Collins <rccproductions@mac.com>
2016-09-16 16:03:01 -0400

    Fix: import routes correctly serverside

--- routes/index.js
+++ routes/index.js
@@ -69,7 +69,7 @@ exports = module.exports = function (app) {
   app.use(express.static('./public'));
 
   app.use((req, res) => {
-    match({ routes: routes(), location: req.url },
+    match({ routes, location: req.url },
       (error, redirectLocation, renderProps) => {
         if (error) {
           res.status(500).send(error.message);
