d97e6abdfea3cf8a959f6f81f35488b35f852548
Ryan Collins <rccproductions@mac.com>
2016-09-16 17:37:57 -0400

    Feat: add routes as object versus jsx

--- app/src/utils/routes.jsx
+++ app/src/utils/routes.jsx
@@ -3,6 +3,7 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from '../store/store';
 import ReduxToastr from 'react-redux-toastr';
+import App from '../components/App';
 
 if (typeof module !== 'undefined' && module.require) {
   if (typeof require.ensure === 'undefined') {
@@ -10,109 +11,94 @@ if (typeof module !== 'undefined' && module.require) {
   }
 }
 
-export const routes = () => (
-  <Route
-    path="/"
-    name="home"
-    getComponent={(location, callback) => {
+export const routes = () => ({
+  component: App,
+  path: '/',
+  indexRoute: {
+    getComponent(location, callback) {
       require.ensure([], (require) => {
-        const App = require('../components/App');
-        callback(null, App);
+        const LandingPage = require('../containers/LandingPage/LandingPage');
+        callback(null, LandingPage);
       });
-    }}
-  >
-    <IndexRoute
-      getComponent={(location, callback) => {
-        require.ensure([], (require) => {
-          const LandingPage = require('../containers/LandingPage/LandingPage');
-          callback(null, LandingPage);
-        });
-      }}
-    />
-    <Route
-      path="portfolio"
-      name="portfolio"
-      getComponent={(location, callback) => {
+    }
+  },
+  childRoutes: [
+    {
+      path: 'portfolio',
+      getComponent(location, callback) {
         require.ensure([], (require) => {
           const Portfolio = require('../containers/Portfolio/Portfolio');
           callback(null, Portfolio);
         });
-      }}
-    />
-    <Route
-      path="projects/:projectId"
-      name="singleproject"
-      getComponent={(location, callback) => {
+      }
+    },
+    {
+      path: 'projects/:projectId',
+      getComponent(location, callback) {
         require.ensure([], (require) => {
           const SingleProjectContainer = require(
             '../containers/SingleProjectContainer/SingleProjectContainer'
           );
           callback(null, SingleProjectContainer);
         });
-      }}
-    />
-    <Route
-      path="contact"
-      name="contact"
-      getComponent={(location, callback) => {
+      }
+    },
+    {
+      path: 'contact',
+      getComponent(location, callback) {
         require.ensure([], (require) => {
           const Contact = require('../containers/Contact/Contact');
           callback(null, Contact);
         });
-      }}
-    />
-    <Route
-      path="blog"
-      name="blog"
-      getComponent={(location, callback) => {
+      }
+    },
+    {
+      path: 'blog',
+      getComponent(location, callback) {
         require.ensure([], (require) => {
           const PostListView = require('../containers/Blog/PostListView/PostListView');
           callback(null, PostListView);
         });
-      }}
-    />
-    <Route
-      path="blog/posts/:postId"
-      name="singlepost"
-      getComponent={(location, callback) => {
+      }
+    },
+    {
+      path: 'blog/posts/:postId',
+      getComponent(location, callback) {
         require.ensure([], (require) => {
           const SinglePostView = require('../containers/Blog/SinglePostView/SinglePostView');
           callback(null, SinglePostView);
         });
-      }}
-    />
-    <Route
-      path="resume/view"
-      name="resume"
-      getComponent={(location, callback) => {
+      }
+    },
+    {
+      path: 'resume/view',
+      getComponent(location, callback) {
         require.ensure([], (require) => {
           const ResumePDF = require('../containers/ResumePDF/ResumePDF');
           callback(null, ResumePDF);
         });
-      }}
-    />
-    <Route
-      path="services"
-      name="services"
-      getComponent={(location, callback) => {
+      }
+    },
+    {
+      path: 'services',
+      getComponent(location, callback) {
         require.ensure([], (require) => {
           const ServicesPage = require('../containers/ServicesPage/ServicesPage');
           callback(null, ServicesPage);
         });
-      }}
-    />
-    <Route
-      path="*"
-      name="notfound"
-      getComponent={(location, callback) => {
+      }
+    },
+    {
+      path: 'notfound',
+      getComponent(location, callback) {
         require.ensure([], (require) => {
           const NotFound = require('../containers/NotFound/NotFound');
           callback(null, NotFound);
         });
-      }}
-    />
-  </Route>
-);
+      }
+    }
+  ]
+});
 
 const RouterApp = (props) => (
   <Provider {...props} store={store}>
